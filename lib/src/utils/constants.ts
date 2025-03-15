07882fd004bd49e6167c80bbf0efbc12cf0d68d5
Oche <ejembioche50@gmail.com>
2025-03-15 08:53:05 +0100

    feat: add getConfig()

--- lib/src/utils/constants.ts
+++ lib/src/utils/constants.ts
@@ -13,3 +13,18 @@ export const ADDRESSES = {
     BRIDGE_MANAGER: "0x0f234D3fD7f1C525fA03afbF0e8c8DE8c1fF79A3",
   },
 };
+
+export function getConfig() {
+  return {
+    evm: {
+      l1Manager: "0x2B7f5d9982d4f7AD67Adb0F7a29D2147E78572ac"
+    },
+    starknet: {
+      executor:
+        "0x7982df3d605fb36e6269c620d647c959c5e49836ae82c398fa7d1675d0d22bf",
+      receiver:
+        "0x4f5033eee9e31bc32c71a48849d97743de5229bf9b9a099f15db3e6758e1ce9"
+    }
+  };
+}
+
